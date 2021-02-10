75c2da917433b681fa66ece52c6174bbb3e948b0
Robert Kis <rkis@partssource.com>
2021-02-10 16:37:46 +0100

    Final Commit

---
+++ urlParse/Models/Url.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace urlParse.Models
+{
+    class Url
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        public string Token { get; set; }
+        public string Host { get; set; }
+        public string SessionId { get; set; }
+        public string Language { get; set; }
+
+        public Url(string url)
+        {
+            Uri myUri = new Uri(url);
+            Id = int.Parse(HttpUtility.ParseQueryString(myUri.Query).Get("id"));
+            FirstName = HttpUtility.ParseQueryString(myUri.Query).Get("name").Split(' ')[0];
+            LastName = HttpUtility.ParseQueryString(myUri.Query).Get("name").Split(' ')[1];
+            Age = string.IsNullOrEmpty(HttpUtility.ParseQueryString(myUri.Query).Get("age")) ? (int?)null : int.Parse(HttpUtility.ParseQueryString(myUri.Query).Get("age"));
+            Token = HttpUtility.ParseQueryString(myUri.Query).Get("token");
+            Host = HttpUtility.ParseQueryString(myUri.Query)[0];
+            SessionId = HttpUtility.ParseQueryString(myUri.Query).Get("session_id");
+            string culture = string.IsNullOrEmpty(HttpUtility.ParseQueryString(myUri.Query).Get("lang")) ? "en-GB" : HttpUtility.ParseQueryString(myUri.Query).Get("lang");
+            Language = CultureInfo.GetCultureInfo(culture).EnglishName;
+        }
+    }
+}
---
+++ urlParse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using urlParse.Models;
+
+namespace urlParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string url = @"https://example.com/page?id=35&name=Harry%20Hacker&token=FA33CD1AA2C345AF&session_id=72AC43F2-F22E-4554-9CAB-42D893EAFCCF&lang=sv-SE";
+            Console.WriteLine("Please insert the URL");
+            string url = Console.ReadLine();
+            Url data = new Url(url);
+
+            Console.WriteLine($"{ data.LastName}, {data.FirstName} - {data.Language}");
+        }
+    }
+}
